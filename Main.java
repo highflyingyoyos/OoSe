aa98ef1d1f6ef4a2dff0e0f48dd2073ed836ecfe
PeterAltmaier <43538742+PeterAltmaier@users.noreply.github.com>
2019-04-21 19:37:53 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("Wie ist dein Name? ");
+        String name = Input.readString();
+        System.out.println("Hallo " + name + "!");
+    }
+}
