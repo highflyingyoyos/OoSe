1dd4597251182072eb031850f3a88603765f54dd
SihdiST <sihdi.st@gmail.com>
2019-06-04 11:33:31 +0200

    new DoubleList

--- DoubleList.java
+++ DoubleList.java
@@ -1,82 +1,151 @@
-package com.company;
-
 public class DoubleList {
-    private DoubleNode head = null;
 
-    public void add(double d){
-        DoubleNode pNode = new DoubleNode(d);
-        if(head == null)
-            head = pNode;
-        else{
-            DoubleNode current = head;
-            while(current.getNext() != null)
-                current = current.getNext();
-            current.setNext(pNode);
-        }
-    }
+	private DoubleNode current;
+	private DoubleNode head;
+	private DoubleNode last;
 
-    public void insertFirst(double d){
-        DoubleNode pNode = new DoubleNode(d);
-        if(head == null)
-            head = pNode;
-        else{
-            pNode.setNext(head);
-            head = pNode;
-        }
-    }
+	private class DoubleNode {
 
-    public double get(int i){
-        //i wird wie bei array von 0 an gezählt
-        if(i < 0){
-            System.out.println("Der Index i ist negativ und ungültig! 0 zurückgegeben");
-            return 0;
-        }
-        DoubleNode current = head;
-        for(int j = 0; j < i; j++){
-            if(current.getNext() == null){
-                System.out.println("Der index i überschreitet die Liste! 0 zurückgegeben");
-                return 0;
-            }
-            current = current.getNext();
-        }
-        return current.getValue();
-    }
+		private double value;
+		private DoubleNode previous;
+		private DoubleNode next;
 
-    public String toString(){
-        StringBuilder retString = new StringBuilder();
-        if(head == null)
-            return retString.toString();
-        else{
-            DoubleNode current = head;
-            retString.append(current.getValue());
-            while(current.getNext() != null){
-                current = current.getNext();
-                retString.append(";");
-                retString.append(current.getValue());
-            }
-            return retString.toString();
-        }
-    }
+		public DoubleNode(double pValue, DoubleNode pNext, DoubleNode pPrevious) {
+			value = pValue;
+			next = pNext;
+			previous = pPrevious;
+		}
 
-    public void remove(int i){
-        if( i < 0)
-            System.out.println("Fehler, Index ist negativ!");
-        else{
-            if(i == 0)
-                head = head.getNext();
-            else{
-                DoubleNode current = head;
-                DoubleNode prev = null;
-                for(int j = 1; j <= i; j++){
-                    prev = current;
-                    current = current.getNext();
-                    if(current == null){
-                        System.out.println("Fehler Index ist zu groß!");
-                        return;
-                    }
-                }
-                prev.setNext(current.getNext());
-            }
-        }
-    }
+		public DoubleNode(double pValue, DoubleNode pNext) {
+			value = pValue;
+			next = pNext;
+		}
+
+		public DoubleNode(double pValue) {
+			value = pValue;
+		}
+
+		public DoubleNode(DoubleNode pNext) {
+			next = pNext;
+		}
+
+		public void setNext(DoubleNode pNext) {
+			next = pNext;
+		}
+
+		public void setPrevious(DoubleNode pPrevious) {
+			next = pPrevious;
+		}
+
+		public void setValue(double pValue) {
+			value = pValue;
+		}
+
+		public DoubleNode getNext() {
+			return next;
+		}
+
+		public DoubleNode getPrevious() {
+			return previous;
+		}
+
+		public double getValue() {
+			return value;
+		}
+	}
+
+	public DoubleList() {
+
+	}
+
+	public DoubleList(DoubleNode pHead) {
+		head = pHead;
+		last = head;
+		current = head;
+	}
+
+	public void add(double pValue) {
+		DoubleNode node = new DoubleNode(pValue);
+
+		last.setNext(node);
+		node.setPrevious(last);
+		last = node;
+
+	}
+
+	public void insertFirst(double pValue) {
+		DoubleNode toInsert = new DoubleNode(pValue, head);
+		head.setPrevious(toInsert);
+		head = toInsert;
+
+	}
+
+	public void insert(double pValue, int pN) {
+		setCurrent(pN);
+
+		DoubleNode toInsert = new DoubleNode(pValue, current.getNext(), current.getPrevious());
+		current.setNext(toInsert);
+		toInsert.getNext().setPrevious(toInsert);
+	}
+
+	public double get(int pN) {
+		setCurrent(pN);
+
+		return getCurrent();
+	}
+
+	public void remove(int pN) {
+		setCurrent(pN);
+
+		current.getPrevious().setNext(current.getNext());
+		current.getNext().setPrevious(current.getPrevious());
+		current = current.getNext();
+	}
+
+	public double getCurrent() {
+		return current.getValue();
+	}
+
+	public void setCurrent(int pN) {
+		current = head;
+
+		if (pN >= 0) {
+			for (int i = 0; i < pN; i++) {
+				if (current.getNext() != null) {
+					current = current.getNext();
+				} else {
+					setCurrent(pN - i);
+				}
+			}
+
+		} else {
+			current = last;
+
+			for (int i = -1; i > pN; i--) {
+				if (current.getPrevious() != null) {
+					current = current.getPrevious();
+				} else {
+					setCurrent(pN - i);
+				}
+			}
+
+		}
+	}
+
+	public String toString() {
+		StringBuilder retString = new StringBuilder();
+		if (head == null)
+			return retString.toString();
+		else {
+			current = head;
+			retString.append(current.getValue());
+			while (current.getNext() != null) {
+				current = current.getNext();
+				retString.append(";");
+				retString.append(current.getValue());
+			}
+			return retString.toString();
+		}
+	}
+
 }
