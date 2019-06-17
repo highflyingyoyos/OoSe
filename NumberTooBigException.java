b7603b8bdfcae51152ef4d3a10cad8430fc1ccbe
SihdiST <sihdi.st@gmail.com>
2019-06-17 23:46:07 +0200

    Delete NumberTooBigException.java

--- NumberTooBigException.java
+++
@@ -1,7 +0,0 @@
-public class NumberTooBigException extends RuntimeException {
-
-    public NumberTooBigException(double number){
-        System.out.println("Fehler: die Zahl " + number +
-                " ist zu groß!");
-    }
-}
